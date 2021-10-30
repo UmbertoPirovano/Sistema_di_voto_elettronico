65a7ca0b754991a0dc77fd7753004fbed69c0cf9
m4ttia <mattia.garavaglia1@studenti.unimi.it>
2021-10-30 16:07:42 +0200

    Creazione metodi votes in Utente (ereditati da Elettore e Amministratore).

--- Sistema_voto_elettronico/src/dominio/Utente.java
+++ Sistema_voto_elettronico/src/dominio/Utente.java
@@ -13,22 +13,40 @@ import java.util.Date;
  *
  */
 abstract class Utente {
-	private final int UserId;
+	private final int userId;
 	private final String name;
 	private final String surname;
-	private final Date birth_date;
 	
 	/**
 	 * Crea un nuovo oggetto utente.
 	 * @param id Identificatore univoco di this assegnato dal sistema.
 	 * @param name Nome dell'utente.
 	 * @param surname Cognome dell'utente.
-	 * @param birth_date Data di nascita dell'utente.
 	 */
-	Utente(int id, String name, String surname, Date birth_date){
-		
+	Utente(int id, String name, String surname){
+		userId = id;
+		this.name = name;
+		this.surname = surname;
 	}
 	
+	/**
+	 * Restituisce un iteratore su tutte le votazioni.
+	 * @return Un iteratore di votazioni. 
+	 */
+	abstract Iterator<Votazione> votes();
 	
+	/**
+	 * Restituisce un iteratore su tutte le votazioni nell'anno specificato.
+	 * @param year Un anno.
+	 * @return Un iteratore di votazioni.
+	 */
+	abstract Iterator<Votazione> votes(int year);
 	
+	/**
+	 * Restituisce un iteratore su tutte le votazioni nel mese e nell'anno specificato. 
+	 * @param month Un mese dell'anno.
+	 * @param year Un anno.
+	 * @return Un iteratore di votazioni.
+	 */
+	abstract Iterator<Votazione> votes(int month, int year);
 }

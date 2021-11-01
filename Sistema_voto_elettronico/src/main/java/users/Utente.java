a7b18b244dec40f44e0c3649d35148aec707fc69
m4ttia <mattia.garavaglia1@studenti.unimi.it>
2021-11-01 14:13:06 +0100

    Modifica alla descrizione della classe

--- Sistema_voto_elettronico/src/main/java/users/Utente.java
+++ Sistema_voto_elettronico/src/main/java/users/Utente.java
@@ -1,14 +1,13 @@
 package users;
 
 import java.util.Date;
+import java.util.Iterator;
 
 /**
  * @author Mattia Garavaglia
  * @author Umberto Pirovano
  * 
  * Un utente generico del sistema di voto elettronico.
- * Questo tipo di utente può visualizzare e selezionare le votazioni, effettuare una votazione, esprimere la volontà
- * di votare a distanza, visualizzare il proprio storico delle votazioni ed effettuare il logout.
  * Le istanze di questa classe sono mutabili.
  *
  */

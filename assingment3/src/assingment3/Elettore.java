7ca1d4adcbe769b3f192f886281626b1ef8875d8
UmbertoPirovano <umberto.pirovano@studenti.unimi.it>
2021-11-19 20:22:37 +0100

    Debugging, ora JML funziona.

--- assingment3/src/assingment3/Elettore.java
+++ assingment3/src/assingment3/Elettore.java
@@ -1,16 +1,16 @@
 package assingment3;
-import java.util.Calendar;
+import java.sql.Date;
 import java.util.regex.Pattern;
+
 public class Elettore {
 	private /*@ spec_public @*/ final String nome, cognome;
 	private /*@ spec_public @*/ int gg, mm, aa;
-	private final /*@ spec_public @*/ String nazione, comune; 
+	private /*@ spec_public @*/ final String nazione, comune; 
 	private /*@ spec_public @*/ char sesso;
-	private final /*@ spec_public @*/ char [] code;
+	private /*@ spec_public @*/ final char [] code;
 	private /*@ spec_public @*/ boolean voto;
 	
-	/*@ public invariant nome != null && cognome != null && (sesso == 'M' || sesso == 'F') && maggiorenne(gg, mm, aa) && (stringEquality(toLowerCase(nazione),"italia") ==> comune != null) && checkFiscalCode(code) 
-	 && match(nome, cognome, gg, mm, aa, nazione, comune, sesso, code); @*/
+	/*@ public invariant nome != null && cognome != null && (sesso == 'M' || sesso == 'F') && (equalStrings(nazione, "italia") ==> comune != null) && (validDate(gg, mm, aa)) && validateTaxCode();@*/
 	
 	public Elettore(String nome, String cognome, int gg, int mm, int aa, String nazione, String comune, char sesso, String code){
 		this.nome = nome;
@@ -25,73 +25,23 @@ public class Elettore {
 		voto = false;
 	}
 	
-	//@ requires voto == false;
-	//@ ensures voto == true;
+	//@ requires (voto == false && isAdult(gg, mm, aa));
+	//@ ensures (voto == true);
 	public void esprimi_voto(){
 		voto = true;
 		return;
 	}
 	
 	/**
-	 * Restituisce una copia della stringa s dalla quale sono state rimosse le vocali.
-	 * @param s
-	 * @return Una copia di s dalla quale sono state rimosse le vocali. 
+	 * Genera i primi 11 caratteri del codice fiscale a partire dai dati anagrafici forniti come argomento.
 	 */
-	//@ requires s != null;
-	//@ ensures \result != null;
-	public static String removeVowels(String s){
-		String s_cons = s.replace("A", "");
-		s_cons = s_cons.replace("E", "");
-		s_cons = s_cons.replace("I", "");
-		s_cons = s_cons.replace("O", "");
-		s_cons = s_cons.replace("U", "");
-		return s_cons;
-	}
-	
-	/**
-	 * Restituisce true se il carattere c corrisponde ad una vocale, false altrimenti.
-	 * @param c
-	 * @return true se c è una vocale, false altrimenti.
-	 */
-	public static boolean isVowel(char c){
-		if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
-			return true;
-		return false;
-	}
-	
-	/**
-	 * Restituisce una copia della stringa s dalla quale sono state rimosse le consonanti.
-	 * @param s
-	 * @return Una copia di s dalla quale sono state rimosse le consonanti.
-	 */
-	public static String removeConsonants(String s){
-		String s_vo = "";
-		for(int i = 0 ; i < s.length() ; i++){
-			if(isVowel(s.charAt(i))) s_vo += s.charAt(i);
-		}
-		return s_vo;
-	}
-	
-	/**
-	 * Restituisce la porzione di codice fiscale calcolata tramite nome, cognome, data di nascita e sesso.
-	 * @param nome
-	 * @param cognome
-	 * @param gg
-	 * @param mm
-	 * @param aa
-	 * @param sesso
-	 * @return La porzione di codice fiscale.
-	 */
-	public static String getCode(String nome, String cognome, int gg, int mm, int aa, char sesso){
-		String name_cons = nome.toUpperCase(); //Conterrà il nome formattato per il codice fiscale
-		String surname_cons = cognome.toUpperCase(); //Conterrà il cognome formattato per il codice fiscale
-		String name_vo = "", surname_vo = "";
+	public static String generateTaxCode(String name, String surname, int gg, int mm, int aa, char sesso){
+		surname = surname.toUpperCase(); //Conterrà il cognome formattato per il codice fiscale
+		name = name.toUpperCase(); //Conterrà il nome formattato per il codice fiscale
 		
-		name_cons = removeVowels(name_cons);
-		surname_cons = removeVowels(surname_cons);
-		name_vo = removeConsonants(nome);
-		surname_vo = removeConsonants(cognome);
-		
+		//primi tre caratteri
+		String surname_cons = removeVowels(surname);
+		String surname_vo = removeConsonants(surname);
 		if(surname_cons.length() > 3) 
 			surname_cons= surname_cons.substring(0, 3); 
 		int j = 0;
@@ -102,6 +52,9 @@ public class Elettore {
 				surname_cons += "X";
 		}
 		
+		//successivi 3 caratteri
+		String name_cons = removeVowels(name);
+		String name_vo = removeConsonants(name);
 		if(name_cons.length() > 3)
 			name_cons = name_cons.charAt(0) + "" + name_cons.charAt(2) + "" + name_cons.charAt(3);
 		j = 0;
@@ -110,9 +63,12 @@ public class Elettore {
 				name_cons += name_vo.charAt(j++) ;
 			else
 				name_cons += "X";
-		}			
+		}
 		
+		//2 caratteri per lánno
 		String year = String.valueOf(aa).substring(2); //Conterrà l'anno formattato per il codice fiscale
+
+		//lettera per il mese
 		char month = ' ';
 		switch(mm){
 			case 1:
@@ -152,6 +108,7 @@ public class Elettore {
 				month = 'T';
 		}
 		
+		//due caratteri per il giorno di nascita
 		String day = "";
 		if(sesso == 'M'){
 			if(gg < 10) day += "0" + gg;
@@ -162,24 +119,21 @@ public class Elettore {
 		}
 			
 			
-		return surname_cons + "" + name_cons + "" + year + "" + month + "" + day; 
+		return surname_cons + "" + name_cons + "" + year + "" + month + "" + day;
 	}
 	
 	/**
-	 * Valuta se i primi 11 caratteri del codice fiscale passato come parametro matchano la parte di codice fiscale ottenuta tramite nome, cognome, data di nascita e sesso.
-	 * @param nome
-	 * @param cognome
-	 * @param gg
-	 * @param mm
-	 * @param aa
-	 * @param sesso
-	 * @param code
-	 * @return true se i primi 11 caratteri di code matchano con quelli ricavati tramite gli altri parametri, false altrimenti.
+	 * Restituisce true se il codice fiscale fornito inizialmente è compatibile con i dati anagrafici inseriti, false altrimenti.
 	 */
-	private /*@ spec_public @*/ /*@ pure @*/ static boolean match(String nome, String cognome, int gg, int mm, int aa, String nazione, String comune, char sesso, char [] code){
-		//controllo primi 11 caratteri
-		boolean flag = String.valueOf(code).substring(0, 11).equals(getCode(nome, cognome, gg, mm, aa, sesso));
+	private /*@ spec_public pure helper @*/ boolean validateTaxCode(){
+		//controllo formato
+		boolean flag = checkTaxCodeFormat(code);
 		if(!flag) return false;
+		
+		//controllo primi 11 caratteri
+		flag = String.valueOf(code).substring(0, 11).equals(generateTaxCode(nome, cognome, gg, mm, aa, sesso));
+		if(!flag) return false;
+		
 		//controllo semplificato degli ultimi 5 caratteri
 		flag = Character.isLetter(code[11]);
 		if(nazione != "IT" && nazione != "Italia" && nazione != "italia" && nazione != "Italy") flag = (code[11] == 'Z');
@@ -190,31 +144,39 @@ public class Elettore {
 		return flag;		
 	}
 	
-	/**
-	 * Valuta se tra la data passata come parametro e quella attuale sono trascorsi almeno 18 anni. 
-	 * @param gg
-	 * @param mm
-	 * @param aa
-	 * @return true se la differenza delle date e' di almeno 18 anni, false altrimenti.
-	 */
-	private /*@ spec_public @*/ /*@ pure @*/ static boolean maggiorenne(int gg, int mm, int aa){
-		Calendar age = Calendar.getInstance();
-		age.clear();
-		age.set(aa, mm, gg);
-		Calendar current = Calendar.getInstance();
-		
-		double mAge = age.getTimeInMillis();
-		double mCurr = current.getTimeInMillis();
-		
-		return mCurr - mAge >= 18.0 * 365.0 * 24.0 * 60.0 * 60.0 * 1000.0;		
+	//Restituisce true se il formato del codice fiscale inserito è corretto.
+	private static boolean checkTaxCodeFormat(char [] code){
+		String regexp = "[A-Z]{6}[0-9]{2}[A-E|H|L|M|P|R-T][01-31|41-71]{2}[A-Z][0-9]{3}[A-Z]";
+		CharSequence seq = java.nio.CharBuffer.wrap(code);
+		return Pattern.matches(regexp, seq);
 	}
+	
+	@SuppressWarnings("deprecation")
+	public /*@ pure @*/ static boolean isAdult(int gg, int mm, int aa){
+		Date present = new Date(2021, 11-1, 20);
+		if(gg == present.getDate() && mm == present.getMonth() && present.getYear() - aa == 18) return true;
+		Date birth = new Date(aa+18, mm-1, gg);
+		return birth.before(present);
+	}
+	
+	/**
+	 * Restituisce true se la data fornita come argomento è antecedente a quella corrente, false altrimenti.
+	 */
+	@SuppressWarnings("deprecation")
+	private /*@ spec_public pure helper @*/ static boolean validDate(int gg, int mm, int aa){
+		return new Date(aa, mm, gg).before(new Date(2021, 11, 20));
+	}
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	/**
 	 * Converte la stringa passata come parametro in un array di caratteri.
 	 * @param s
 	 * @return L'array caratteri ottenuto da s.
 	 */
-	private /*@ spec_public @*/ /*@ pure @*/ static char [] toCharArray(String s){
+	//@ requires (s != null);
+	//@ ensures (\result != null);
+	public static char [] toCharArray(String s){
 		char[] ch = new char[s.length()];
 
 	    for (int i = 0; i < s.length(); i++) {
@@ -225,37 +187,56 @@ public class Elettore {
 	}
 	
 	/**
-	 * Valuta se il codice fiscale passato come parametro e' formattato correttamente. 
-	 * @param code
-	 * @return true se code e' formattato correttamente, false altrimenti.
-	 */
-	private /*@ spec_public @*/ /*@ pure @*/ static boolean checkFiscalCode(char [] code){
-		String regexp = "[A-Z]{6}[0-9]{2}[A-E|H|L|M|P|R-T][01-31|41-71]{2}[A-Z][0-9]{3}[A-Z]";
-		CharSequence seq = java.nio.CharBuffer.wrap(code);
-		return Pattern.matches(regexp, seq);
-	}
-    
-	/**
-	 * Rende minuscoli tutti i caratteri alfabetici della stringa passata come parametro
+	 * Restituisce una copia della stringa s dalla quale sono state rimosse le vocali.
 	 * @param s
-	 * @return La stringa s con tutti i caratteri alfabetici minuscoli.
+	 * @return Una copia di s dalla quale sono state rimosse le vocali. 
 	 */
-	private /*@ spec_public @*/ /*@ pure @*/ static String toLowerCase(String s){
-		return s.toLowerCase();
+	//@ requires (s != null);
+	//@ ensures (\result != null);
+	public static String removeVowels(String s){
+		String s_cons = s.replace("A", "");
+		s_cons = s_cons.replace("E", "");
+		s_cons = s_cons.replace("I", "");
+		s_cons = s_cons.replace("O", "");
+		s_cons = s_cons.replace("U", "");
+		return s_cons;
 	}
 	
 	/**
-	 * Esegue il confronto tra due stringhe a livello logico.
-	 * @param s1
-	 * @param s2
-	 * @return true se il contenuto di s1 e' lo stesso di s2, false altrimenti.
+	 * Restituisce true se il carattere c corrisponde ad una vocale, false altrimenti.
+	 * @param c
+	 * @return true se c è una vocale, false altrimenti.
 	 */
-	private /*@ spec_public @*/ /*@ pure @*/ static boolean stringEquality(String s1, String s2){
+	public static boolean isVowel(char c){
+		if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+			return true;
+		return false;
+	}
+	
+	/**
+	 * Restituisce una copia della stringa s dalla quale sono state rimosse le consonanti.
+	 * @param s
+	 * @return Una copia di s dalla quale sono state rimosse le consonanti.
+	 */
+	//@ requires (s != null);
+	//@ ensures (\result != null);
+	public static String removeConsonants(String s){
+		String s_vo = "";
+		for(int i = 0 ; i < s.length() ; i++){
+			if(isVowel(s.charAt(i))) s_vo += s.charAt(i);
+		}
+		return s_vo;
+	}
+	
+	/**
+	 * Restituisce true se le due stringhe sono uguali.
+	 */
+	private /*@ spec_public pure helper @*/ static boolean equalStrings(String s1, String s2){
 		return s1.equals(s2);
 	}
 	
 	@Override
 	public String toString() {
-		return "" + nome + " " + cognome + ", sesso: " + sesso + ", nato a: " + comune + " il: " + gg + "/" + mm + "/" + aa + "\n codice fiscale: " + new String(code);
+		return "" + nome + " " + cognome + ", sesso: " + sesso + ", nato a: " + comune + " il: " + gg + "/" + mm + "/" + aa + "\n codice fiscale: " + new String(code) + "\n voto: " + voto;
 	}
 }
--- assingment3/src/assingment3/Main.java
+++ assingment3/src/assingment3/Main.java
@@ -1,11 +1,10 @@
 package assingment3;
-
 public class Main {
-
 	public static void main(String[] args) {
-		Elettore io = new Elettore("Rossi", "Mario", 10, 12, 1985, "italia", "San Giuliano Terme", 'M', "RSSMRA85T10A562S");
-		System.out.println(io);
+		Elettore io = new Elettore("Mario", "Rossi", 10, 12, 1985, "italia", "San Giuliano Terme", 'M', "RSSMRA85T10A562S");
 		io.esprimi_voto();
+		System.out.println(io);
+		//System.out.println("Codice fiscale valido: " + io.validateTaxCode());
+		System.out.println("Maggiorenne: " + Elettore.isAdult(10, 12, 1985));
 	}
-
 }

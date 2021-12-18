24a55f4a3605c4feea60f3d2ce7974a9b4eafeaa
m4ttia <mattia.garavaglia1@studenti.unimi.it>
2021-12-18 15:42:00 +0100

    Modifica nel metodo main

--- assignment5/src/Main.java
+++ assignment5/src/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-    	ConnectToDb conn = new ConnectToDb();
+    	ConnectToDb conn = new ManageUserConnection();
     	launch();
     }
 

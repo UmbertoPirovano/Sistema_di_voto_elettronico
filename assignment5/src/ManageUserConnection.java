d3379a0ae255b18a0d1bc74a404d61139d29eb06
m4ttia <mattia.garavaglia1@studenti.unimi.it>
2021-12-18 15:40:54 +0100

    Creata classe che estende ConnectToDb. Gestisce le modifiche agli user da parte di un admin

---
+++ assignment5/src/ManageUserConnection.java
@@ -0,0 +1,29 @@
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ManageUserConnection extends ConnectToDb {
+	
+	public ManageUserConnection() {
+		super();
+	}
+	
+	public void addUser(String username, String password, String userMode) {
+		password = MD5(password);
+		
+		PreparedStatement st = null;
+		try {
+			if(userMode.equals("elettore")) {
+				st = con.prepareStatement("INSERT INTO elettore(cF,password) VALUES (?,?);");
+			}else if(userMode.equals("amministratore")) {
+				st = con.prepareStatement("INSERT INTO amministratore(username,password) VALUES (?,?);");
+			}else {
+				throw new IllegalArgumentException("Undefined user mode: "+ userMode);
+			}
+			st.setString(1, username);
+			st.setString(2, password);
+			st.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}

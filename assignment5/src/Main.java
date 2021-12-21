62ae843e4cd2833a86130f625c193a5079458a1d
UmbertoPirovano <umberto.pirovano@studenti.unimi.it>
2021-12-21 14:31:10 +0100

    Implementazione pattern DAO.

--- assignment5/src/Main.java
+++ assignment5/src/Main.java
@@ -1,8 +1,12 @@
+import dbConnection.UserDAO;
+import dbConnection.UserDAOImpl;
 import gui.LoginWindowView;
 
 public class Main {
 
 	public static void main(String[] args) {		
+		UserDAO dbDao = new UserDAOImpl();
+		dbDao.insertUser(new Elettore(), null)
 		LoginWindowView.show();
 	}
 

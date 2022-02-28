67acea2fd72520391b5a7f42e594f1632b46a6a3
pirou <pirou@DESKTOP-NNDIN6E.fritz.box>
2022-02-28 18:35:29 +0100

    Creazione della schermata di prenotazione.

---
+++ Sistema_voto_elettronico/src/main/java/gui/bookPollController.java
@@ -0,0 +1,100 @@
+package gui;
+
+import java.io.IOException;
+
+import dbConnection.PollDAO;
+import dbConnection.PollDAOImpl;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import poll.Votazione;
+import system.Sessione;
+import users.User;
+
+public class bookPollController {
+
+    @FXML
+    private Label label_nomeVotazione;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private Text nameSurnameLabel;
+
+    @FXML
+    private Button noButton;
+
+    @FXML
+    private Text usernameLabel;
+
+    @FXML
+    private Button yesButton;
+
+    @FXML
+    void confirm(ActionEvent event) {
+    	PollDAO p = new PollDAOImpl();
+    	p.book(Sessione.getSessione().utente, Sessione.getSessione().getVotazione());
+    	showPollSelection();
+    }
+
+    @FXML
+    void deny(ActionEvent event) {
+    	showPollSelection();
+    }
+
+    @FXML
+    void logout(ActionEvent event) {
+    	Sessione.getSessione().logoutUser();
+    	showLoginWindow();
+    }
+    
+    @FXML
+    void initialize() {
+    	User u = Sessione.getSessione().utente;
+    	nameSurnameLabel.setText(u.getName() + " " + u.getSurname());
+    	usernameLabel.setText(u.getUsername());
+    	
+    	Votazione v = Sessione.getSessione().getVotazione();
+    	label_nomeVotazione.setText(v.getNome());    	
+    }
+    
+    void showLoginWindow() {
+    	try {
+			logoutButton.getScene().getWindow().hide();
+    		Parent root = FXMLLoader.load(getClass().getResource("LoginWindow.fxml"));
+            Stage stage = new Stage();
+        	stage.setTitle("Sistema di voto elettronico - Login");
+        	stage.setScene(new Scene(root, 500, 390));
+        	stage.setResizable(false);
+        	stage.show();
+		}catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+    }
+    
+    /**
+     * Chiude la schermata corrente ed apre la schermata di selezione votazione dedicata agli Elettori.
+     */
+    void showPollSelection() {
+    	try {
+			yesButton.getScene().getWindow().hide();
+    		Parent root = FXMLLoader.load(getClass().getResource("PollSelection.fxml"));
+            Stage stage = new Stage();
+        	stage.setTitle("Sistema di voto elettronico - Selezione votazioni");
+        	stage.setScene(new Scene(root, 900, 780));
+        	stage.setResizable(false);
+        	stage.show();
+		}catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+    }
+
+}
+

746cff6d54f36a5630e4858b6edefe8d3f520eb3
m4ttia <mattia.garavaglia1@studenti.unimi.it>
2022-03-29 11:21:28 +0200

    Eccezione che gestisce il tentativo di visualizzare il contenuto di una scheda bianca.

---
+++ Sistema_voto_elettronico/src/main/java/vote/VoidCardException.java
@@ -0,0 +1,8 @@
+package vote;
+/**
+ * Questa eccezione viene sollevata se si tenta di visualizzare il risultato di una scheda elettorale bianca,
+ * ovvero senza preferenze espresse al suo interno.
+ */
+public class VoidCardException extends RuntimeException {
+	
+}

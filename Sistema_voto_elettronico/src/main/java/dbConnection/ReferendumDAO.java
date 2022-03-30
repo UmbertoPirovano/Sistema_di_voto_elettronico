3776a2df5a0c153715aba4c070bfae12a035e0e3
m4ttia <mattia.garavaglia1@studenti.unimi.it>
2022-03-30 10:45:49 +0200

    Implementazione metodo vota.

--- Sistema_voto_elettronico/src/main/java/dbConnection/ReferendumDAO.java
+++ Sistema_voto_elettronico/src/main/java/dbConnection/ReferendumDAO.java
@@ -1,5 +1,10 @@
 package dbConnection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import poll.Referendum;
 import poll.Votazione;
 import poll.VotazioneStandard;
@@ -8,11 +13,39 @@ import vote.VotoReferendum;
 import vote.VotoOrdinale;
 
 public class ReferendumDAO implements VoteDAO{
+	
+	Connection con = null;
+
+	private static Connection getConnection() {
+		Connection c = null;
+		try {
+			c = DriverManager.getConnection("jdbc:mysql://localhost/sistema_di_voto?user=root&password=");
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return c;
+	}
 
 	@Override
 	public boolean vota(Votazione v, Voto voto) {
 		if(!(v instanceof Referendum) || !(voto instanceof VotoReferendum))
 			throw new IllegalArgumentException();
+		con = getConnection();
+
+		try {
+			PreparedStatement st = con.prepareStatement("INSERT INTO voti_referendum(votazione, scelta) VALUES(?, ?);");
+			st.setInt(1, v.getId());
+			if(voto.schedaBianca()) {
+				st.setString(2, "NULL");
+			}else {
+				st.setString(2, Boolean.toString(((VotoReferendum) voto).getVoto()));
+			}
+			st.executeUpdate();
+			return true;
+		}catch(SQLException se) {
+			se.printStackTrace();
+		}
+		
 		return false;
 	}
 
